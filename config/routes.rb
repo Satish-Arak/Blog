f85f9f2c1219d65c734096ab00e1cf61eb5e9bed
Satish-Arak <vickyarak@gmail.com>
2021-12-11 13:39:43 +0530

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-root'welcome#index'
-
- resources :articles do
-  resources :comments
- end
- 
+  devise_for :users
+  root'welcome#index'
+  get 'users/index'
+  resources :articles do
+    resources :comments
+  end
 end 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
